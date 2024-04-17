707595d378790ee266d311bdb8ee8e3793d7592d
kdotom <kopongmensah@gmail.com>
2024-04-17 12:55:38 -0400

    adding custom verilog

--- src/project.v
+++ src/project.v
@@ -1,24 +1,35 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+`timescale 1ns/1ns
+module project (
+    input wire clk,
+    input wire reset,
+    output reg [7:0] out
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+reg [2:0] counter;
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        counter <= 3'b000;
+    end else begin
+        if (counter < 3'b101) begin
+            counter <= counter + 1;
+        end else begin
+            counter <= 3'b000;
+        end
+    end
+end
+
+always @(*) begin
+    case (counter)
+        3'b000: out = 8'b10010000;
+        3'b001: out = 8'b00011000;
+        3'b010: out = 8'b01001000;
+        3'b011: out = 8'b01100000;
+        3'b100: out = 8'b00100100;
+        3'b101: out = 8'b10000100;
+        default: out = 8'b00000000;
+    endcase
+end
 
 endmodule
